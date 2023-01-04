89c7ea6545f482e1f70e2ad1861e0e436feee1df
Javier Bratos <javierbratosmonje@gmail.com>
2023-01-04 22:23:53 +0100

    Team getters and setters

--- LigaFantasy/src/bean/Team.java
+++ LigaFantasy/src/bean/Team.java
@@ -6,41 +6,65 @@ public class Team {
 	private int points;
 	private int goalF;// goles a favor
 	private int goalC;// goles en contra
-
+	
+	
+	/**
+	 * 
+	 */
 	public Team() {
-
+		goalF = 0;
+		goalC = 0;
+		points = 0;
 	}
-
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
-
+	/**
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * @return the points
+	 */
 	public int getPoints() {
 		return points;
 	}
-
+	/**
+	 * @param points the points to set
+	 */
 	public void setPoints(int points) {
 		this.points = points;
 	}
-
+	/**
+	 * @return the goalF
+	 */
 	public int getGoalF() {
 		return goalF;
 	}
-
+	/**
+	 * @param goalF the goalF to set
+	 */
 	public void setGoalF(int goalF) {
 		this.goalF = goalF;
 	}
-
+	/**
+	 * @return the goalC
+	 */
 	public int getGoalC() {
 		return goalC;
 	}
-
+	/**
+	 * @param goalC the goalC to set
+	 */
 	public void setGoalC(int goalC) {
 		this.goalC = goalC;
 	}
 
+	
 }
