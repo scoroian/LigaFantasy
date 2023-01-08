5ec2ae3efed8d836caecc3dea18ca7e65aed1000
chapipalos8 <javierpalos2904@gmail.com>
2023-01-08 18:04:01 +0100

    Clase para ordenacion

---
+++ LigaFantasy/src/algoritmes/SortAlgoritmes.java
@@ -0,0 +1,36 @@
+package algoritmes;
+
+import bean.Team;
+
+public class SortAlgoritmes {
+
+	public static Team[] shellSort(Team[] param) {
+		Team[] data = param;
+		int pasada = 1;
+		for (int k = data.length / 2; k >= 1; k = data.length / (2 * pasada)) {
+			for (int j = 0; j + k < data.length; j++) {
+				Team aux = null;
+				int i = j;
+				if (data[i].mayorQue(data[i + k])) {
+					aux = data[i];
+					data[i] = data[i + k];
+					data[i + k] = aux;
+					backTracking(k, i, data);
+				}
+			}
+			pasada++;
+		}
+		return data;
+	}
+
+	private static void backTracking(int gap, int i, Team[] data) {
+		Team aux = null;
+		while (i - gap >= 0 && data[i - gap].mayorQue(data[i])) {
+			aux = data[i];
+			data[i] = data[i - gap];
+			data[i - gap] = aux;
+			i -= gap;
+		}
+	}
+
+}
